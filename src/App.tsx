5973ca1251d7ec965c07ce3d774fbf0a6931e9e3
Ayoola Abimbola <ao.abimbola@gmail.com>
2021-02-20 00:08:37 -0600

    added apollo client and graphQL end point

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const TEST = gql`
+    query{
+        characters {
+            results {
+                name
+            }
+        }
+    }
+`
 
 const App = () => {
+  const { data } = useQuery(TEST);
+  console.log(data);
+
   return (
     <div className="App">
       Rick and Morty
