dd9063e662edcdf87c8fdb594b2f32a3bb37679b
Ayoola Abimbola <ao.abimbola@gmail.com>
2021-02-21 16:31:12 -0600

    navbar link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import './navbar.scss';
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <div className='navbar__ayoola'>ayoola.io</div>
+      <a href='http://ayoola.io/' target='_blank' rel='noreferrer' className='navbar__ayoola'>ayoola.io</a>
       <div className='navbar__title'>Rick and Morty KeyBoard Navigation</div>
     </nav>
   );
